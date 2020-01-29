5fe6cd5665e209c1329126ca8bf48faf4914f55c
Ricardo Morales Morales <rimorales@personalsoft.com.co>
2020-01-29 13:51:52 -0500

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantillaComponent } from './plantilla/plantilla.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+import { EncabezadoComponent } from './plantilla/encabezado/encabezado.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantillaComponent,
+    InicioSesionComponent,
+    PaginaPrincipalComponent,
+    EncabezadoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
